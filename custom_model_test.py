c71f8f3a9a21b2375dd95748f4f106fa3e001c63
JakeEvans6179 <159437717+JakeEvans6179@users.noreply.github.com>
2025-07-18 17:20:44 +0800

    Add files via upload

---
+++ custom_model_test.py
@@ -0,0 +1,38 @@
+from ultralytics import YOLO
+import cv2
+
+#Load YOLOv8 model
+model = YOLO('2dog_onehuman_test.pt')  #Custom model
+
+#0 = default webcam
+capture = cv2.VideoCapture(0)
+
+
+if not capture.isOpened():
+    print("Error: Cannot open webcam")
+    exit()
+
+#Read frames from webcam
+while True:
+    ret, frame = capture.read()
+
+    if not ret:
+        print("Failed to grab frame")
+        break
+
+    #Detection and tracking
+    results = model.track(frame, persist=True)
+
+    #plotting results
+    frame_plot = results[0].plot()
+
+    #frame with detections
+    cv2.imshow('YOLOv8 Webcam', frame_plot)
+
+    #Exit loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#Release resources
+capture.release()
+cv2.destroyAllWindows()
